9670df044891f1f786e50f4711c84ca31dc89ec0
Andrew Pierce <andrunix@gmail.com>
2016-02-26 06:37:02 -0500

    Initial revision

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({
+  host: '127.0.0.1',
+  port: 8000
+});
+
+
+server.register(require('inert'), (err) => {
+  if (err) throw err;
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply) {
+      reply.file('./public/index.html');
+    }
+  });
+});
+
+
+
+server.start((err) => {
+  if (err)
+    throw(err);
+
+  console.log('Server running at: ' + server.info.uri);
+});
