b9174180bf8120cc242655176db84f3dbaefca57
Andrew Pierce <andrunix@gmail.com>
2016-04-29 17:23:41 -0400

    Adding some react and babel.

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+	render: function() {
+						return  (
+							<div>Hello</div>
+							)
+					}
+});
+
+
+ReactDOM.render(
+		<Hello />,
+		document.getElementById('app')
+		)
+
+
