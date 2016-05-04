fb0e8cbb0aaf53de64383698422ab21134bfcabc
Andrew Pierce <andrunix@gmail.com>
2016-05-04 16:53:27 -0400

    Fixes

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const server = new Hapi.Server();
 server.connection({ port: 8000 });
 
 const rootHandler = function(request, reply) {
-	reply.file(__dirname + '/dist/index.html');
+	// reply.file(__dirname + '/dist/index.html');
+	reply.file(__dirname + '/public/index.html');
 };
 
 const aboutHandler = function (request, reply) {
@@ -36,7 +37,7 @@ server.register([require('inert'), require('vision')], (err) => {
 
 	server.route({ method: 'GET', path: '/',      handler: rootHandler });
 	server.route({ method: 'GET', path: '/about', handler: aboutHandler });
-	server.route({ method: 'GET', path: '/{param*}', handler: directory: { path: __/dirname + '/public' } });
+	server.route({ method: 'GET', path: '/{param*}', handler: { directory: { path: __dirname + '/public' } } });
 });
 
 
